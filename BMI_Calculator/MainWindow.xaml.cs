1f603d0c2326660fa3c114789c19e20a53aa624a
Dana Wilson <danalwil@outlook.com>
2020-11-10 11:10:24 -0500

    Part 3 Update

--- BMI_Calculator/MainWindow.xaml.cs
+++ BMI_Calculator/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,23 +13,37 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Data;
 
 namespace BMI_Calculator
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
+    [XmlRoot("BMI_Calculater", Namespace = "www.bmicalc.ninja")]
     public partial class MainWindow : Window
     {
+        public string FilePath = "C:/Users/Wilson_dana/source/repos/BMI_Calculator/";
+        public string FileName = "yourBMI.xml";
 
         public class Customer
         {
+            [XmlAttribute("Last Name")]
             public string lastName { get; set; }
+            [XmlAttribute("First Name")]
             public string firstName { get; set; }
+            [XmlAttribute("Phone Number")]
             public string phoneNumber { get; set; }
+            [XmlAttribute("Height")]
             public int heightInches { get; set; }
+            [XmlAttribute("Weight")]
             public int weightLbs { get; set; }
+            [XmlAttribute("Customer BMI")]
             public int custBMI { get; set; }
+            [XmlAttribute("Status")]
             public string statusTitle { get; set; }
         }
 
@@ -73,7 +88,8 @@ namespace BMI_Calculator
             string yourBMIStatus = "NA";
             customer1.statusTitle = yourBMIStatus;
 
-            //MessageBox.Show($"The Customer's name is {customer1.firstName} {customer1.lastName} and they can be at {customer1.phoneNumber}. They are {customer1.heightInches} inches tall. Their weight is {customer1.weightLbs}. Their BMI is {bmi}.");
+            //MessageBox.Show($"The Customer's name is {customer1.firstName} {customer1.lastName} and they can be at {customer1.phoneNumber}. 
+            //They are {customer1.heightInches} inches tall. Their weight is {customer1.weightLbs}. Their BMI is {bmi}.");
 
             xYourBMIResults.Text = bmi.ToString();
             if (bmi < 18.5)
@@ -96,6 +112,28 @@ namespace BMI_Calculator
                 xBIMMessage.Text = "According to CDC.gov BMI Calculator you are obese.";
                 customer1.statusTitle = "Obese";
             }
+
+            TextWriter writer = new StreamWriter(FilePath + FileName);
+            XmlSerializer ser = new XmlSerializer(typeof(Customer));
+            ser.Serialize(writer, customer1);
+            writer.Close();
+        }
+
+        private void OnLoadStats()
+        {
+            Customer cust = new Customer();
+            XmlSerializer des = new XmlSerializer(typeof(Customer));
+            using (XmlReader reader = XmlReader.Create(FilePath + FileName))
+            {
+                cust = (Customer)des.Deserialize(reader);
+                //MessageBox.Show($"The Customer's name is {cust.firstName} {cust.LlastName} and they can be readed at {cust.phoneNumber}. Their weight is {cust.weightLbs}. Their BMi is {cust.custBMI}");
+                xLastNameBox.Text = cust.lastName;
+                xFirstNameBox.Text = cust.firstName;
+                xPhoneBox.Text = cust.phoneNumber;
+            }
+            DataSet xmlData = new DataSet();
+            xmlData.ReadXml(FilePath + FileName, XmlReadMode.Auto);
+            xDataGrid.ItemsSource = xmlData.Tables[0].DefaultView;
         }
     }
 }
